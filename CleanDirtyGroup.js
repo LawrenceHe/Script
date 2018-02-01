6e0cd1723977a62fca894db5b7f198acdfa57d06
LawrenceHe <helun6286@gmail.com>
2018-02-01 18:01:10 +0800

    请求函数抽象出来

--- CleanDirtyGroup.js
+++ CleanDirtyGroup.js
@@ -16,61 +16,55 @@ rs.pipe(csv())
 
 function cleanDirtyGroup() {
 
-    var request = require('request');
-    var yoyo = "e295095220"
+    // 游游的UID
+    const yoyo = "e295095220"
     // USER("0")普通用户
     // LEADER("1")领队
     // EXPERT("2")达人
     // AGENT("3")客服
     // HOST("5")群主
-    var role = '5'
+    const role = '5'
     var count = 0
 
     for (item in groupinfo) {
-        console.log(item)
         // 移除现有群主
         let jid = groupinfo[item]['group_jid']
         let uid = groupinfo[item]['UID']
-        let USER_DATA = {"groupJid":jid, "userJids":[uid],"operatorname":uid}
-        console.log(USER_DATA);
-        let options = {
-            method: 'POST',
-            url: 'http://webapi.soa.ctripcorp.com/api/11611/removeMember',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(JSON.stringify(USER_DATA))
-            },
-            json: USER_DATA
-        };
-    
-        request(options, function(error, response, body){
-            console.log('removeMember')
-            console.log(error)
-            console.log(body)
-        })
+        let url = 'http://webapi.soa.ctripcorp.com/api/11611/removeMember'
+        let USER_DATA = {"groupJid":jid,"userJids":[uid],"operatorname":uid}
+        console.log(USER_DATA)
+        postRequest(url, USER_DATA)
     
         // 设置游游为群主
-        let USER_DATA1 = {"userJid":yoyo,"groupJid":jid,"role":role,"reason":"yoyo as group owner"}
-        console.log(USER_DATA1);
-        let options1 = {
-            method: 'POST',
-            url: 'http://webapi.soa.ctripcorp.com/api/11611/changeGroupRole',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(JSON.stringify(USER_DATA1))
-            },
-            json: USER_DATA1
-        };
-    
-        request(options1, function(error, response, body){
-            console.log('changeGroupRole')
-            console.log(error)
-            console.log(body)
-        })
-    
+        url = 'http://webapi.soa.ctripcorp.com/api/11611/changeGroupRole'
+        USER_DATA = {"userJid":yoyo,"groupJid":jid,"role":role,"reason":"set yoyo as group owner"}
+        console.log(USER_DATA);
+        postRequest(url, USER_DATA)
+
         count++
-        if (count == 1) {
-            break
-        }
+        // if (count == 20) {
+        //     break
+        // }
     }
+}
+
+function postRequest(url, user_data) {
+    let request = require('request');
+    let options = {
+        method: 'POST',
+        url: url,
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(JSON.stringify(user_data))
+        },
+        json: user_data
+    };
+
+    request(options, function(error, response, body){
+        console.log(url)
+        if (error) {
+            console.log(error)
+        }
+        console.log(body)
+    })
 }
