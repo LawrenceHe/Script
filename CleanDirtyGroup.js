9b64d120cd533d98d036f9b1f48f37593c0e51e5
LawrenceHe <helun6286@gmail.com>
2018-02-01 13:44:42 +0800

    文件读取是异步的，必须在回调中处理数据

--- CleanDirtyGroup.js
+++ CleanDirtyGroup.js
@@ -9,68 +9,68 @@ rs.pipe(csv())
     .on('data', function(data) {
         groupinfo.push(data)
     })
-    .on('end', function() {
-        //console.log(groupinfo)
-        //console.log(groupinfo[0]['UID'])
-        //console.log(groupinfo[0]['group_jid'])
-    })
+    .on('end', cleanDirtyGroup)
     .on('error', function(err){
         console.log(err)
     })
 
-var request = require('request');
-var yoyo = "e295095220"
-// USER("0")普通用户
-// LEADER("1")领队
-// EXPERT("2")达人
-// AGENT("3")客服
-// HOST("5")群主
-var role = '5'
-var count = 0
-for (item in groupinfo) {
+function cleanDirtyGroup() {
 
-    // 移除现有群主
-    let jid = item['group_jid']
-    let uid = item['UID']
-    let USER_DATA = {"groupJid":jid, "userJids":[uid],"operatorname":uid}
-    console.log(USER_DATA);
-    let options = {
-        method: 'POST',
-        url: 'http://webapi.soa.ctripcorp.com/api/11611/removeMember',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(JSON.stringify(USER_DATA))
-        },
-        json: USER_DATA
-    };
+    var request = require('request');
+    var yoyo = "e295095220"
+    // USER("0")普通用户
+    // LEADER("1")领队
+    // EXPERT("2")达人
+    // AGENT("3")客服
+    // HOST("5")群主
+    var role = '5'
+    var count = 0
 
-    request(options, function(error, response, body){
-        console.log('removeMember')
-        console.log(error)
-        console.log(body)
-    })
-
-    // 设置游游为群主
-    let USER_DATA1 = {"userJid":yoyo,"groupJid":jid,"role":role,"reason":"yoyo as group owner"}
-    console.log(USER_DATA1);
-    let options1 = {
-        method: 'POST',
-        url: 'http://webapi.soa.ctripcorp.com/api/11611/changeGroupRole',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(JSON.stringify(USER_DATA1))
-        },
-        json: USER_DATA1
-    };
-
-    request(options1, function(error, response, body){
-        console.log('changeGroupRole')
-        console.log(error)
-        console.log(body)
-    })
-
-    count++
-    if (count == 1) {
-        break
+    for (item in groupinfo) {
+        console.log(item)
+        // 移除现有群主
+        let jid = groupinfo[item]['group_jid']
+        let uid = groupinfo[item]['UID']
+        let USER_DATA = {"groupJid":jid, "userJids":[uid],"operatorname":uid}
+        console.log(USER_DATA);
+        let options = {
+            method: 'POST',
+            url: 'http://webapi.soa.ctripcorp.com/api/11611/removeMember',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(JSON.stringify(USER_DATA))
+            },
+            json: USER_DATA
+        };
+    
+        request(options, function(error, response, body){
+            console.log('removeMember')
+            console.log(error)
+            console.log(body)
+        })
+    
+        // 设置游游为群主
+        let USER_DATA1 = {"userJid":yoyo,"groupJid":jid,"role":role,"reason":"yoyo as group owner"}
+        console.log(USER_DATA1);
+        let options1 = {
+            method: 'POST',
+            url: 'http://webapi.soa.ctripcorp.com/api/11611/changeGroupRole',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(JSON.stringify(USER_DATA1))
+            },
+            json: USER_DATA1
+        };
+    
+        request(options1, function(error, response, body){
+            console.log('changeGroupRole')
+            console.log(error)
+            console.log(body)
+        })
+    
+        count++
+        if (count == 1) {
+            break
+        }
     }
 }
